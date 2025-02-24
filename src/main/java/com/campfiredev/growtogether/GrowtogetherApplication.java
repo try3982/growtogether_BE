87fadcc733e5bfd0025d7159ca6da99457102735
kisunghoon <kisunghoon29@gmail.com>
2025-02-24 12:39:30 +0900

    초기 프로젝트 설정

---
+++ src/main/java/com/campfiredev/growtogether/GrowtogetherApplication.java
@@ -0,0 +1,13 @@
+package com.campfiredev.growtogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowtogetherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrowtogetherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/campfiredev/growtogether/GrowtogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campfiredev.growtogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowtogetherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
