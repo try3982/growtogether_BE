d017a46ec54fa6acfb3878296655d445e09db742
kisunghoon <kisunghoon29@gmail.com>
2025-02-25 09:23:08 +0900

    feat(엔티티 생성): 부트캠프 , 부트캠프 댓글 , 좋아요 엔티티 생성
    부트 캠프 관련 엔티티 생성
    
    1. 부트캠프 엔티티 생성
    2. 부트캠프 댓글 엔티티 생성
    3. 좋아요 엔티티 생성

---
+++ src/main/java/com/campfiredev/growtogether/bootcamp/entity/BootCampReview.java
@@ -0,0 +1,67 @@
+package com.campfiredev.growtogether.bootcamp.entity;
+
+import com.campfiredev.growtogether.bootcamp.type.ProgramCourse;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BootCampReview {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bootCampReviewId;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    private ProgramCourse programCourse;
+
+    @Column(nullable = false)
+    private Long viewCount;
+
+    @Column(nullable = false)
+    private String bootCampName;
+
+    @Column(nullable = false)
+    private LocalDate bootCampStartDate;
+
+    @Column(nullable = false)
+    private LocalDate bootCampEndDate;
+
+    @Column(nullable = false)
+    private Integer learningLevel;
+
+    @Column(nullable = false)
+    private Integer assistantSatisfaction;
+
+    @Column(nullable = false)
+    private Integer likeCount;
+
+    //user 추가 예정
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
