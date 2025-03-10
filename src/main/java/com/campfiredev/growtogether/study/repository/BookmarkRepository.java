928efbd2526b807d3e31f80a901c5e00a078f434
Oh-Myeongjae <omjcws@gmail.com>
2025-03-11 04:45:49 +0900

    feat(스터디 북마크): 스터디 북마크 설정,,해제 기능 개발

---
+++ src/main/java/com/campfiredev/growtogether/study/repository/BookmarkRepository.java
@@ -0,0 +1,12 @@
+package com.campfiredev.growtogether.study.repository;
+
+import com.campfiredev.growtogether.study.entity.Bookmark;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+
+    Optional<Bookmark> findByMember_MemberIdAndStudy_StudyId(Long userId, Long studyId);
+}
+
