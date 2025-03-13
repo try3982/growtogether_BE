fc36b4804b5d38d653f639d0ac0c591a65a501eb
WonJin <try3982@kakao.com>
2025-03-13 16:28:35 +0900

    cors

--- src/main/java/com/campfiredev/growtogether/common/config/SecurityConfig.java
+++ src/main/java/com/campfiredev/growtogether/common/config/SecurityConfig.java
@@ -75,7 +75,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("https://jiangxy.github.io"));
+        configuration.setAllowedOrigins(List.of("https://jiangxy.github.io","http://localhost:3000"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
