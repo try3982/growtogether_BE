96108b0babfcc9f524e1015d203d311984dfbf06
WonJin <try3982@kakao.com>
2025-03-13 16:19:43 +0900

    cors

--- src/main/java/com/campfiredev/growtogether/chat/config/WebSocketConfig.java
+++ src/main/java/com/campfiredev/growtogether/chat/config/WebSocketConfig.java
@@ -5,11 +5,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -21,8 +19,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
     registry.addEndpoint("/ws-chat")
-        .setAllowedOriginPatterns("*")
-        .withSockJS();
+            .setAllowedOriginPatterns("https://jiangxy.github.io")
+            .withSockJS()
+            .setSessionCookieNeeded(false);
   }
 
   @Override
