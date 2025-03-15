36f2235670be420d9a1e12f8224f425cea478c4b
WonJin <try3982@kakao.com>
2025-03-16 00:27:15 +0900

    cors 프론트요청으로 포트번호 변경하였습니다.

--- src/main/java/com/campfiredev/growtogether/common/config/SecurityConfig.java
+++ src/main/java/com/campfiredev/growtogether/common/config/SecurityConfig.java
@@ -87,7 +87,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("https://jiangxy.github.io","http://localhost:3000"));
+       // configuration.setAllowedOrigins(List.of("https://jiangxy.github.io","http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("https://jiangxy.github.io","http://localhost:5173"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
