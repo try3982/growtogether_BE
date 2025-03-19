94a5d37a7c09af9b2d1f776b68be4d4fadf6eb0c
kisunghoon <kisunghoon29@gmail.com>
2025-03-19 22:06:22 +0900

    fix(BootCamp): 댓글 부분 수정
    1. 게시글 전체 조회 시 댓글 개수 까지 같이 노출되도록
    2. 댓글 조 회시, author , createdAt 나오도록 추가

--- src/main/java/com/campfiredev/growtogether/bootcamp/dto/CommentResponseDto.java
+++ src/main/java/com/campfiredev/growtogether/bootcamp/dto/CommentResponseDto.java
@@ -4,6 +4,7 @@ import com.campfiredev.growtogether.bootcamp.entity.BootCampComment;
 import lombok.*;
 import org.springframework.data.domain.Page;
 
+import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
@@ -19,17 +20,19 @@ public class CommentResponseDto {
     private Long commentId;
     private String content;
     private Long userId;
-    private String nickname;
+    private String author;
     private Boolean isDeleted;
     private List<CommentResponseDto> childComments;
+    private LocalDateTime createdAt;
 
     public static CommentResponseDto fromEntity(BootCampComment comment) {
         return CommentResponseDto.builder()
                 .commentId(comment.getBootCampCommentId())
                 .content(comment.getCommentContent())
                 .userId(comment.getMember().getMemberId())
-                .nickname(comment.getMember().getNickName())
+                .author(comment.getMember().getNickName())
                 .isDeleted(comment.getIsDeleted())
+                .createdAt(comment.getCreatedAt())
                 .childComments(Optional.ofNullable(comment.getChildComments())
                         .orElse(Collections.emptyList())
                         .stream()
