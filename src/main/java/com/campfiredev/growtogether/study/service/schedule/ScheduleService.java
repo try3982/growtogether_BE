7e4917d932d9693d4e873e837629f885fa5452d9
focandlol <focandlol@naver.com>
2025-03-22 20:09:31 +0900

    fix: 스케줄 삭제 수정

--- src/main/java/com/campfiredev/growtogether/study/service/schedule/ScheduleService.java
+++ src/main/java/com/campfiredev/growtogether/study/service/schedule/ScheduleService.java
@@ -78,7 +78,8 @@ public class ScheduleService {
     ScheduleEntity save = scheduleRepository.save(
         ScheduleEntity.create(studyMemberEntity, scheduleCreateDto));
 
-    LocalDateTime start = LocalDateTime.of(scheduleCreateDto.getStartDate(), scheduleCreateDto.getStartTime());
+    LocalDateTime start = LocalDateTime.of(scheduleCreateDto.getStartDate(),
+        scheduleCreateDto.getStartTime());
     LocalDateTime end = start.plusMinutes(scheduleCreateDto.getTotalTime());
 
     checkMainConflict(studyMemberEntity.getStudy(), start, end, save.getId());
@@ -164,12 +165,14 @@ public class ScheduleService {
             attendanceMap.getOrDefault(schedule.getId(), new ArrayList<>())
         ))
         .collect(Collectors.groupingBy(
-            (ScheduleAttendeeDto scheduleAttendeeDto) -> scheduleAttendeeDto.getStart().toLocalDate()));
+            (ScheduleAttendeeDto scheduleAttendeeDto) -> scheduleAttendeeDto.getStart()
+                .toLocalDate()));
 
     return ScheduleAttendeeMonthDto.from(groupedSchedules);
   }
 
-  private void checkMainConflict(Study study, LocalDateTime start, LocalDateTime end, Long scheduleId){
+  private void checkMainConflict(Study study, LocalDateTime start, LocalDateTime end,
+      Long scheduleId) {
     List<ScheduleEntity> mainSchedules = scheduleRepository.findByStudyAndStartBetweenAndType(
         study, start, end, MAIN);
 
@@ -198,10 +201,10 @@ public class ScheduleService {
   }
 
   private void validateSameUser(Long memberId, ScheduleEntity scheduleEntity) {
-    StudyMemberEntity studyMemberEntity = getStudyMemberEntity(memberId,
-        scheduleEntity.getStudy().getStudyId());
+    StudyMemberEntity studyMemberEntity = getStudyMemberEntity(
+        scheduleEntity.getStudy().getStudyId(), memberId);
 
-    if (!memberId.equals(studyMemberEntity.getId())) {
+    if (!memberId.equals(studyMemberEntity.getMember().getMemberId())) {
       throw new CustomException(NOT_AUTHOR);
     }
   }
